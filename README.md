dbee74ed8461185ee9d71a1c38e3dc894151eab7
Johannes Lötzsch <git@johannesloetzsch.de>
2025-08-02 20:13:53 +0200

    build trixie with mate

---
+++ README.md
@@ -0,0 +1,15 @@
+# nix toolchain for debian live build
+
+## Usage
+
+`lb` (`debootstrap`) requires root priviledges.
+
+To build a live debian image, run:
+```sh
+sudo nix develop
+```
+
+## Customization
+
+* [Introduction](https://debian-live-config.readthedocs.io/en/latest/custom.html)
+* [Debian Live Manual](https://live-team.pages.debian.net/live-manual/html/live-manual/index.en.html)
--- flake.nix
+++ flake.nix
@@ -15,8 +15,11 @@
         util-linux
         debootstrap
         wget
+        git
       ];
       shellHook = ''
+        set -e
+
         export LIVE_BUILD=$(pwd)
         PATH=$PATH:$LIVE_BUILD/frontend:$LIVE_BUILD/scripts:$LIVE_BUILD/scripts/build
 
@@ -24,14 +27,21 @@
         sed -i 's@/sbin/losetup@${pkgs.util-linux}/bin/losetup@' functions/defaults.sh
         sed -i 's@/usr/sbin/debootstrap@${pkgs.debootstrap}/bin/debootstrap@' scripts/build/bootstrap_debootstrap
         sed -i 's@/usr/bin/wget@${pkgs.wget}/bin/wget@' scripts/build/chroot_firmware
+        sed -i 's@/usr/bin/wget@${pkgs.wget}/bin/wget@' scripts/build/installer_debian-installer
+        git update-index --assume-unchanged functions/* scripts/build/*
 
-        LB_SECURITY="false"  ## Hotfix E: The repository 'http://security.debian.org bookworm/updates Release' does not have a Release file.
-        lb config --distribution bookworm --security $LB_SECURITY
+        LB_DISTRIBUTION="trixie"
+        LB_SECURITY="false"  ## Hotfix E: The repository 'http://security.debian.org $LB_DISTRIBUTION/updates Release' does not have a Release file.
+        lb config --distribution $LB_DISTRIBUTION --security $LB_SECURITY
 
         lb clean
-        lb build
+        lb build || (lb clean --purge; lb build)
 
         ls -lh *.iso
+        echo
+        echo "To test the image, you may want run:"
+        echo "qemu-system-x86_64 -m 4G -boot d -cdrom *.iso"
+        exit
       '';
     };
   };
